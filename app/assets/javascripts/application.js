bde131d3db40a2299cdd3fbb82ae072158227a85
Jose Manuel Rodriguez <jmrv002@gmail.com>
2018-09-19 07:14:35 -0400

    add query

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
 //= require_tree .
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+console.log('d')
